470ddb68768fb917bad05177e00cc98310495f04
zalfarmdhn <zalfarmdhn1011@gmail.com>
2024-10-13 12:27:52 +0700

    setup

---
+++ src/components/ComponentItem.jsx
@@ -0,0 +1,7 @@
+export default function ComponentItem() {
+  return (
+    <div>
+      <h1>Component Item</h1>
+    </div>
+  );
+}
---
+++ src/components/ComponentList.jsx
@@ -0,0 +1,7 @@
+export default function ComponentList() {
+  return (
+    <div>
+      <h1>Map Component Item Here</h1>
+    </div>
+  )
+}
