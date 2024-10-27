8c5ba3eddd0b45358cd25cce117ef9f211841e88
Muhamad Jamaludin <muhhjam@gmail.com>
2024-10-27 16:32:17 +0700

    update

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // eslint-disable-next-line no-undef
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function registration(payload) {
   return await axios.post(`${BASE_URL}/auth/registration`, payload).catch((error) => {
@@ -10,6 +10,12 @@ export async function registration(payload) {
 }
 
 export async function verifyOtp(payload) {
+  return await axios.post(`${BASE_URL}/auth/verify-otp`, payload).catch((error) => {
+    return error;
+  });
+}
+
+export async function change(payload) {
   return await axios.post(`${BASE_URL}/auth/verify-otp`, payload).catch((error) => {
     return error;
   });
