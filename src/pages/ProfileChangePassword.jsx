6171abcaecf0859cc15c3c6f15243a144ef8c97f
Muhamad Jamaludin <muhhjam@gmail.com>
2024-12-09 22:57:02 +0700

    udpate

--- src/pages/ProfileChangePassword.jsx
+++ src/pages/ProfileChangePassword.jsx
@@ -63,7 +63,7 @@ export default function ProfilePage() {
     return (
       <div className="container-sm mx-auto p-4">
         <h1 className="text-2xl text-revamp-neutral-8 font-medium">
-          Data Akun Anda
+          Ubah Kata Sandi
         </h1>
         <div className="mt-4 rounded-md border p-4 border-revamp-neutral-6">
         <InputPassword
