7e07db86d4835fd494cf93efde307fbcfed11868
Muhamad Jamaludin <muhhjam@gmail.com>
2025-01-01 21:43:22 +0700

    update

--- src/components/Modal/ModalWaiting/index.jsx
+++ src/components/Modal/ModalWaiting/index.jsx
@@ -9,7 +9,7 @@ export default function ModalWaiting({ setIsWaiting }) {
   return (
     <div className="fixed left-0 right-0 top-0 h-[100dvh] z-[99] flex justify-center items-center">
       <div className="w-full h-[100dvh] bg-black opacity-[0.8]"></div>
-      <div className="fixed p-[12px] flex-col bg-white rounded-md justify-center items-center h-max-[500px] w-[400px] overflow-x-auto">
+      <div className="fixed px-[12px] py-[26px] flex-col bg-white rounded-md justify-center items-center h-max-[500px] max-w-[600px] min-w-[300px] w-[600px] overflow-x-auto">
         <h1 className="text-2xl font-semibold text-center mb-8">
           Menunggu Konfirmasi dari Admin
         </h1>
@@ -17,22 +17,24 @@ export default function ModalWaiting({ setIsWaiting }) {
           src={WaitingApproval}
           alt="Preview"
           fallbackSrc={IcEmptyFile}
-          className="w-[400px] h-[400px] object-cover"
+          className="w-[316px] object-cover mx-auto"
         />
+        <div className="w-full flex justify-center">
         <button
           type="button"
-          className="bg-revamp-secondary-500 w-full py-[8px] px-[46px] text-white text-[14px] font-[600] rounded-[15px]"
+          className="bg-revamp-secondary-500 w-fit py-[8px] px-[46px] text-white text-[14px] font-[600] rounded-[15px]"
           onClick={() => setIsWaiting(true)}
-        >
+          >
           Ubah Kembali
         </button>
+        </div>
         <div className="flex justify-center items-center mt-[10px]">
           <span className="text-revamp-neutral-10 font-[500] text-[14px]">
             Anda mau ganti akun?{" "}
             <a
               onClick={handleLogout}
               className="text-revamp-error-300 cursor-pointer"
-            >
+              >
               Logout
             </a>
           </span>
